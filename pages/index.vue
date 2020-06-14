27c86fb0dbe3b0b5de669bafc6f123c47d50ed69
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2020-06-14 08:03:04 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">
+        admin
+      </h1>
+      <h2 class="subtitle">
+        My finest Nuxt.js project
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+      <a href="#" class="a-button-buy-again">Buy Now</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style></style>
